70cdf6b44c16d57cab75d4a1661d51d5faf9fc0d
manami0624 <manana624@yahoo.co.jp>
2023-05-28 20:51:28 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,14 +47,25 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it "passwordは半角英数字混合でないと登録できない" do
+      it "passwordは半角英数字混合でないと登録できない（数字のみ）" do
         @user.password = "123456"
         @user.password_confirmation = "123456"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password must include both letters and numbers")
       end
+      it "passwordは半角英数字混合でないと登録できない(英字のみ)" do
+        @user.password = "abcdef"
+        @user.password_confirmation = "abcdef"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password must include both letters and numbers")
+      end
+      it "全角文字を含むパスワードは登録できない" do
+        @user.password = "１２３４５６"
+        @user.password_confirmation = "１２３４５６"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password must include both letters and numbers")
+      end
       it "passwordとpassword_confirmationが一致していないと登録できない" do
-        # @user.password = 'password'
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
