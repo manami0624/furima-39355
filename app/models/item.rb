691f5dea92c84e58d42271b7a2f4cab8b5254f2f
manami0624 <manana624@yahoo.co.jp>
2023-05-29 23:19:54 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   has_one    :order
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :user, :item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, :price, presence: true
+
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id,  numericality: { other_than: 1 , message: "can't be blank"} 
 end
