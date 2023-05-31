29bf01f5e2d10e94cba7b6057c5592ae7a16efbe
manami0624 <manana624@yahoo.co.jp>
2023-05-31 11:59:32 +0900

    item_price.js修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,6 +7,6 @@ priceInput.addEventListener("input", () => {
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
-  const profit = document.getElementById("profit")
-  profit.innerHTML = Math.floor(inputValue * 0.9);
+  const profitDom = document.getElementById("profit")
+  profitDom.innerHTML = Math.floor(inputValue * 0.9);
 })})
