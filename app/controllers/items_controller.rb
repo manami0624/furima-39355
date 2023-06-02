2f988d12564925e8c307458cff94a7c736d688f4
manami0624 <manana624@yahoo.co.jp>
2023-06-02 16:24:28 +0900

    URL直接入力で編集に遷移できなくする実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
      @items = Item.order('created_at DESC')
@@ -24,11 +26,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    move_to_index unless current_user.id == @item.user_id
   end
 
   def update
     @item = Item.find(params[:id])
-    if @item.update(item_params)
+    if @item.user_id == current_user.id && @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -38,6 +41,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    # redirect_to action: :index unless current_user.id == item.user_id
+    redirect_to root_path unless item.user_id == current_user.id
+
   end
 
 end
