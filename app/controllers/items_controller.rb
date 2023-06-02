aa2df1bdc61b2772636d373c17828b5c0699b24f
manami0624 <manana624@yahoo.co.jp>
2023-06-02 14:39:18 +0900

    updateアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,9 +21,14 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
   
   private
