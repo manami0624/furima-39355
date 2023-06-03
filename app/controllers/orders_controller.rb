3bfbe348d2004fc31875c2550581d03b03844f8a
manami0624 <manana624@yahoo.co.jp>
2023-06-03 17:00:19 +0900

    購入のためのフォーム実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
+    if current_user.id != @item.user_id && @item.order == nil
+    @items = Item.all
     @order_form = OrderForm.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     @order_form = OrderForm.new(order_params)
+    if  @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_form).permit(:user_id, :item_id, :zipcode, :prefecture_id, :city, :street, :building, :phone)
   end
 
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,12 +1,23 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :zipcode, :prefecture_id, :city, :street, :building, :phone, :token
+  attr_accessor :user_id, :item_id, :zipcode, :prefecture_id, :city, :street, :building, :phone
   
-  validates :zipcode, :prefecture_id, :city, :street, :phone, presence: true
+  with_options presence: true do
+    validates :zipcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'input correctly' }
+    validates :phone, format: { with: /\A\d{10,11}\z/, message: "input only number" }
+    validates :prefecture_id, :city, :street
+  end
 
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShoppingAddress.create(
+        zipcode: zipcode,
+        prefecture_id: prefecture_id,
+        city: city,
+        street: street,
+        building: building,
+        phone: phone,)
   end
 
 end
