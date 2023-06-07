1a15c7ff40d27fff5a823266e6a6d9ad19c62751
manami0624 <manana624@yahoo.co.jp>
2023-06-07 13:51:04 +0900

    編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,36 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.3"
+lock '3.17.3'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'furima-39355'
+
+set :repo_url,  'git@github.com:manami0624/furima-39355.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/manami0624.pem'] 
+
+
+                                  set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
+
+# config valid for current version and patch releases of Capistrano
+# lock "~> 3.17.3"
+
+# set :application, "my_app_name"
+# set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
